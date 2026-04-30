172b3a158ad8fa643ceb85319b2cf7bc36f9efaf
Yisus538 <jmanuelmartinez2003@gmail.com>
2026-04-29 23:59:33 -0300

    done

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,14 +1,14 @@
-import { createBrowserRouter } from "react-router";
-import { LoginPage } from "../page/LoginPage";
-import { ForgotPasswordPage } from "../page/ForgotPasswordPage";
+import { createBrowserRouter } from 'react-router-dom';
+import { LoginPage } from '../page/LoginPage';
+import { ForgotPasswordPage } from '../page/ForgotPasswordPage';
 
 export const AppRouter = createBrowserRouter([
   {
-    path: "/",
-    element: <LoginPage />,
+    path: '/',
+    element: <LoginPage />
   },
   {
-    path: "/forgot-password",
-    element: <ForgotPasswordPage />,
+    path: '/forgot-password',
+    element: <ForgotPasswordPage />
   },
 ]);
